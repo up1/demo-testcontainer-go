b26f11fa25fa22868d877df6ffbeeca3258bf91b
up1 <somkiat.p@gmail.com>
2019-08-04 22:22:14 +0700

    Example with nginx

---
+++ nginx_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestNginxLatestReturn(t *testing.T) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "nginx",
+		ExposedPorts: []string{"80/tcp"},
+		WaitingFor:   wait.ForHTTP("/"),
+	}
+	nginxC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	defer nginxC.Terminate(ctx)
+	ip, err := nginxC.Host(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+	port, err := nginxC.MappedPort(ctx, "80")
+	if err != nil {
+		t.Error(err)
+	}
+	resp, err := http.Get(fmt.Sprintf("http://%s:%s", ip, port.Port()))
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d. Got %d.", http.StatusOK, resp.StatusCode)
+	}
+}
